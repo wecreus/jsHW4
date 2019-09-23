d186bd25d34efa93a218e64f0273af9578dace3d
wecreus <wecreus@gmail.com>
2019-09-23 03:38:27 +0300

    all is working with gradient

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+
+
+
+
+
+
+
+
+
+
 let canvas = document.getElementById('myCanvas');
 let ctx = canvas.getContext('2d');
 canvas.width = window.innerWidth;
@@ -22,93 +32,125 @@ canvas.height = window.innerHeight;
 // Math.random() * (350 - 280) + 280)       x- y+
 
 
-let radius = 50;
-let iX = 0;
-let iY = 0;
-let speed = 10;
-let dxX = Math.round( (speed * (Math.sin(135 * Math.PI / 180))) * 1000) / 1000; // base velocity
-let dxY = -Math.round( (speed * (Math.cos(135 * Math.PI / 180))) * 1000) / 1000;
-let iteration = 0; // used to skip frame check if circle just appeared
-let iteration2 = -1; // used to skip frame checking after we applied new angles (reduces circle jiggling)
+function Circle(radius, color1, color2) {
+    this.radius = radius;
+    this.color1 = color1;
+    this.color2 = color2;
+    this.iX = 0;
+    this.iY = 0;
+    this.speed = 9;
+    this.dxX = Math.round( (this.speed * (Math.sin(135 * Math.PI / 180))) * 1000) / 1000; // base velocity
+    this.dxY = -Math.round( (this.speed * (Math.cos(135 * Math.PI / 180))) * 1000) / 1000;
+    this.iteration = 0; // used to skip frame check if circle just appeared
+    this.iteration2 = -1; // used to skip frame checking after we applied new angles (reduces circle jiggling)
+    this.angle = 0;
+    this.drawCircle = function() {
+        ctx.beginPath();
 
+        let grd = ctx.createLinearGradient(this.iX -  this.radius, this.iY - this.radius, this.iX + this.radius, this.iY + this.radius);
+        grd.addColorStop(0, color1);
+        grd.addColorStop(1, color2);
+        ctx.fillStyle = grd;
 
+        ctx.arc(this.iX, this.iY, this.radius, 0, 2 * Math.PI, false);
+        ctx.fill();
+    };
+    this.update = function () {
+        if (this.iteration > (this.radius / this.dxX) && this.iteration2 < 0) { // frame checking
 
-function draw() {
-    ctx.clearRect(0, 0, window.innerWidth, window.innerHeight);
-    ctx.beginPath();
+            if ((this.iX + this.radius) >= window.innerWidth) {
 
-    ctx.fillStyle = "blue";
-
-
-
-
-
-    ctx.arc(iX, iY, radius, 0, 2 * Math.PI, false);
-    ctx.fill();
-
-    if (iteration > (radius / dxX) && iteration2 < 0) { // frame checking
-        let angle;
-        if ((iX + radius) >= window.innerWidth) {
-
-            if (dxY > 0) {
-                angle = Math.ceil(Math.random() * (260 - 190) + 190);
-            } else {
-                angle = Math.ceil(Math.random() * (350 - 280) + 280);
+                if (this.dxY > 0) {
+                    this.angle = Math.ceil(Math.random() * (260 - 190) + 190);
+                } else {
+                    this.angle = Math.ceil(Math.random() * (350 - 280) + 280);
+                }
+                this.dxX = Math.round( (this.speed * (Math.sin(this.angle * Math.PI / 180))) * 1000) / 1000;
+                this.dxY = -Math.round( ( this.speed * (Math.cos(this.angle * Math.PI / 180))) * 1000) / 1000;
+                this.iteration2 = 4;
             }
-            dxX = Math.round( (speed * (Math.sin(angle * Math.PI / 180))) * 1000) / 1000;
-            dxY = -Math.round( ( speed * (Math.cos(angle * Math.PI / 180))) * 1000) / 1000;
-            iteration2 = 4;
+            if ((this.iY + this.radius) >= window.innerHeight) {
+
+                if (this.dxX > 0) {
+                    this.angle = Math.ceil(Math.random() * (80 - 10) + 10);
+                } else {
+                    this.angle = Math.ceil(Math.random() * (350 - 280) + 280);
+                }
+
+                this.dxX = Math.round( (this.speed * (Math.sin(this.angle * Math.PI / 180))) * 1000) / 1000;
+                this.dxY = -Math.round( ( this.speed * (Math.cos(this.angle * Math.PI / 180))) * 1000) / 1000;
+
+                this.iteration2 = 4;
+
+            }
+            if ((this.iX - this.radius) <= 0) {
+
+
+                if (this.dxY > 0) {
+                    this.angle = Math.ceil(Math.random() * (170 - 100) + 100);
+                } else {
+                    this.angle = Math.ceil(Math.random() * (80 - 10) + 10);
+                }
+
+                this.dxX = Math.round( (this.speed * (Math.sin(this.angle * Math.PI / 180))) * 1000) / 1000;
+                this.dxY = -Math.round( (this.speed * (Math.cos(this.angle * Math.PI / 180))) * 1000) / 1000;
+
+                this.iteration2 = 4;
+
+            }
+            if ((this.iY - this.radius) <= 0) {
+
+                if (this.dxX > 0) {
+                    this.angle = Math.ceil(Math.random() * (170 - 100) + 100);
+                } else {
+                    this.angle = Math.ceil(Math.random() * (260 - 190) + 190);
+                }
+
+                this.dxX = Math.round( (this.speed * (Math.sin(this.angle * Math.PI / 180))) * 1000) / 1000;
+                this.dxY = -Math.round( (this.speed * (Math.cos(this.angle * Math.PI / 180))) * 1000) / 1000;
+                this.iteration2 = 4;
+            }
         }
-        if ((iY + radius) >= window.innerHeight) {
-
-            if (dxX > 0) {
-                angle = Math.ceil(Math.random() * (80 - 10) + 10);
-            } else {
-                angle = Math.ceil(Math.random() * (350 - 280) + 280);
-            }
-
-            dxX = Math.round( (speed * (Math.sin(angle * Math.PI / 180))) * 1000) / 1000;
-            dxY = -Math.round( ( speed * (Math.cos(angle * Math.PI / 180))) * 1000) / 1000;
-
-            iteration2 = 4;
-
-        }
-        if ((iX - radius) <= 0) {
 
 
-            if (dxY > 0) {
-                angle = Math.ceil(Math.random() * (170 - 100) + 100);
-            } else {
-                angle = Math.ceil(Math.random() * (80 - 10) + 10);
-            }
+        this.iX += this.dxX;
+        this.iY += this.dxY;
+        this.iteration++;
+        this.iteration2--;
+        this.drawCircle();
+    };
+}
 
-            dxX = Math.round( (speed * (Math.sin(angle * Math.PI / 180))) * 1000) / 1000;
-            dxY = -Math.round( (speed * (Math.cos(angle * Math.PI / 180))) * 1000) / 1000;
-
-            iteration2 = 4;
-
-        }
-        if ((iY - radius) <= 0) {
-
-            if (dxX > 0) {
-                angle = Math.ceil(Math.random() * (170 - 100) + 100);
-            } else {
-                angle = Math.ceil(Math.random() * (260 - 190) + 190);
-            }
-
-            dxX = Math.round( (speed * (Math.sin(angle * Math.PI / 180))) * 1000) / 1000;
-            dxY = -Math.round( (speed * (Math.cos(angle * Math.PI / 180))) * 1000) / 1000;
-            iteration2 = 4;
+function myFu() {
+    let colorR1 = Math.random() * (256 - 2) + 2;
+    let colorB1 = Math.random() * (256 - 2) + 2;
+    let colorG1 = Math.random() * (256 - 2) + 2;
+    let colorR2 = Math.random() * (256 - 2) + 2;
+    let colorB2 = Math.random() * (256 - 2) + 2;
+    let colorG2 = Math.random() * (256 - 2) + 2;
+    let circle = new Circle(Math.ceil(Math.random() * (50 - 20) + 20), `rgb(${colorR1}, ${colorB1}, ${colorG1})`, `rgb(${colorR2}, ${colorB2}, ${colorG2})`);
+    arrayOfCircles.push(circle);
+    function draw() {
+        requestAnimationFrame(draw);
+        ctx.clearRect(0, 0, window.innerWidth, window.innerHeight);
+        for(let i = 0; i < arrayOfCircles.length; i++){
+            arrayOfCircles[i].update();
         }
     }
-
-
-    iX += dxX;
-    iY += dxY;
-    iteration++;
-    iteration2--;
-    requestAnimationFrame(draw);
+    if(!arrayOfCircles[1]){draw();}
 }
-draw();
 
+let sI = 2;
+let circlesAmount = 20;
+let arrayOfCircles = [];
+
+myFu();
+let myInterval = setInterval(function () {
+    myFu();
+    if(sI === circlesAmount){
+        clearInterval(myInterval);
+    }
+    sI++;
+    console.log(arrayOfCircles);
+}, 5000);
 
